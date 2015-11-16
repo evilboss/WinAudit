9e5a823e01827cfa2df8d68f43461505fe8e6c70
Jr Reyes <jr.evilboss@gmail.com>
2015-11-16 18:45:52 +0800

    Added new sample files

---
+++ app/client/templates/leaderboard/leaderboard.js
@@ -0,0 +1,32 @@
+/*****************************************************************************/
+/* Leaderboard: Event Handlers */
+/*****************************************************************************/
+Template.Leaderboard.events({
+
+});
+
+/*****************************************************************************/
+/* Leaderboard: Helpers */
+/*****************************************************************************/
+Template.Leaderboard.helpers({
+
+});
+
+/*****************************************************************************/
+/* Leaderboard: Lifecycle Hooks */
+/*****************************************************************************/
+Template.Leaderboard.created = function () {
+};
+
+Template.Leaderboard.rendered = function () {
+};
+
+Template.Leaderboard.destroyed = function () {
+};
+Template.player.helpers({
+
+});
+
+Template.player.events({
+
+});
